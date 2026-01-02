f2c3b0b8815adb0985381af49f2e71599312ee17
Allison Rodrigues <allison_rps@hotmail.com>
2026-01-02 18:23:07 -0300

    partidas rankeadas

---
+++ CalculadoraPartidasRankeadas.js
@@ -0,0 +1,40 @@
+// Função que calcula o nível do herói
+function calcularRank(vitorias, derrotas) {
+  let saldoVitorias = vitorias - derrotas;
+  let nivel = "";
+
+  // Estrutura de decisão
+  if (vitorias < 10) {
+    nivel = "Ferro";
+  } else if (vitorias >= 11 && vitorias <= 20) {
+    nivel = "Bronze";
+  } else if (vitorias >= 21 && vitorias <= 50) {
+    nivel = "Prata";
+  } else if (vitorias >= 51 && vitorias <= 80) {
+    nivel = "Ouro";
+  } else if (vitorias >= 81 && vitorias <= 90) {
+    nivel = "Diamante";
+  } else if (vitorias >= 91 && vitorias <= 100) {
+    nivel = "Lendário";
+  } else {
+    nivel = "Imortal";
+  }
+
+  // Retorno do resultado
+  return {
+    saldoVitorias: saldoVitorias,
+    nivel: nivel
+  };
+}
+
+// Variáveis
+let vitorias = 87;
+let derrotas = 20;
+
+// Chamada da função
+let resultado = calcularRank(vitorias, derrotas);
+
+// Saída
+console.log(
+  `O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`
+);
